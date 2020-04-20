f50f2b85ffa044335f7ac422021538274ec8c208
Ziya Bayramov <ziya.bayramov.1@yandex.com>
2020-04-20 20:28:28 +0400

    fill js

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const choices = ["rock", "scissors", "paper"];
+
+const computer_choice = (array_of_choices) => array_of_choices[Math.floor((Math.random()*array_of_choices.length))];
+
+const decide_winner = (p1, p2) => {
+    /**
+     * 0 - tie
+     * 1 - p1
+     * 2 - p2
+    */
+    switch(p1) {
+        case "rock":
+            return p2 == "scissors" ? 1 : (p2 == "paper") ? 2 : 0
+            break;
+        case "scissors":
+            return p2 == "paper" ? 1 : (p2 == "rock") ? 2 : 0
+            break;
+        case "paper":
+            return p2 == "rock" ? 1 : (p2 == "scissors") ? 2 : 0
+            break;
+        default:
+            alert("something wrong");
+    }
+}
+
+let user_choice = prompt("Rock, scissors, paper? ");
+let computer_choice = computer_choice(choices);
+
+let result = decide_winner(user_choice, computer_choice);
+
+if(result == 1) {
+    alert("Human wins");
+} else if (result == 2) {
+    alert("Computer wins");
+} else if(result ==0 ) {
+    alert("Tie");
+} else {
+    alert("Something wrong")
+}
+
